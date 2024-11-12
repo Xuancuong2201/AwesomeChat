9e98ede8b83424d2fc88a2a877414fe120b7515a
XuanCuong2910 <ctemon2201@gmail.com>
2024-11-12 17:45:37 +0700

    First update

---
+++ app/src/main/java/com/example/awesomechat/interact/InfoFieldQuery.kt
@@ -0,0 +1,34 @@
+package com.example.awesomechat.interact
+
+class InfoFieldQuery {
+    companion object {
+        const val COLLECTION_USER = "User"
+        const val COLLECTION_FRIEND = "Friend"
+        const val COLLECTION_CONVERSATION = "Conversation"
+        const val COLLECTION_MESSAGES = "Messages"
+        const val KEY_FRIEND = "friend"
+        const val KEY_STATE = "state"
+        const val KEY_SIDE_A = "sideA"
+        const val KEY_SIDE_B = "sideB"
+        const val KEY_EMAIL = "email"
+        const val KEY_USER = "user"
+        const val KEY_RECIPIENT = "recipient"
+        const val KEY_MESSAGE = "Message"
+        const val KEY_TIME = "time"
+        const val KEY_TYPE = "type"
+        const val KEY_URL = "url"
+        const val KEY_STATUS = "status"
+        const val KEY_SENT_BY = "sentby"
+        const val TYPE_IMAGE = "image"
+        const val TYPE_MULTI = "multi image"
+        const val TYPE_DOUBLE = "double"
+        const val STATE_REQUEST = "request"
+        const val STATE_FRIEND = "friend"
+        const val STATE_USER = "user"
+        const val STATE_INVITATION = "invitation"
+        const val URL_DEFAULT =
+            "https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg"
+        const val TYPE_MESS = "mess"
+        const val MULTI_IMAGE = "multi image"
+    }
+}
